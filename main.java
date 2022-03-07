5bbd36a321913921c284a396b2ccb1dd656db26e
hojatifar <68428907+hojatifar@users.noreply.github.com>
2022-03-07 03:16:06 -0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,8 @@
+/** 
+ * The Test App class that simply prints "Hi Bye" to standard output.
+ */
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hi Bye");
+  }
+}
